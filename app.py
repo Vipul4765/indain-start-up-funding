dd2e972b3592c305c0c64f531ee2fc1be0159f7c
Vipul Kumar <vipuldhankar17170277@gmail.com>
2023-12-11 00:59:32 +0530

    final

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator
 
-st.set_page_config(layout='wide',page_title='StartUp Analysis')
+st.set_page_config(layout='wide', page_title='StartUp Analysis')
 
 df = pd.read_csv('startup_cleaned.csv')
-df['date'] = pd.to_datetime(df['date'],errors='coerce')
+df['date'] = pd.to_datetime(df['date'], errors='coerce')
 df['month'] = df['date'].dt.month
 df['year'] = df['date'].dt.year
 
@@ -21,21 +22,21 @@ def load_overall_analysis():
     # total funded startups
     num_startups = df['startup'].nunique()
 
-    col1,col2,col3,col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        st.metric('Total',str(total) + ' Cr')
+        st.metric('Total', str(total) + ' Cr')
     with col2:
         st.metric('Max', str(max_funding) + ' Cr')
 
     with col3:
-        st.metric('Avg',str(round(avg_funding)) + ' Cr')
+        st.metric('Avg', str(round(avg_funding)) + ' Cr')
 
     with col4:
-        st.metric('Funded Startups',num_startups)
+        st.metric('Funded Startups', num_startups)
 
     st.header('MoM graph')
-    selected_option = st.selectbox('Select Type',['Total','Count'])
+    selected_option = st.selectbox('Select Type', ['Total', 'Count'])
     if selected_option == 'Total':
         temp_df = df.groupby(['year', 'month'])['amount'].sum().reset_index()
     else:
@@ -43,12 +44,21 @@ def load_overall_analysis():
 
     temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
 
-    fig3, ax3 = plt.subplots()
+    fig3, ax3 = plt.subplots(figsize=(10, 6))
     ax3.plot(temp_df['x_axis'], temp_df['amount'])
+    ax3.set_xlabel('Month-Year')
+    ax3.set_ylabel('Total Amount' if selected_option == 'Total' else 'Count')
+    ax3.set_title('Month-wise Analysis')
+
+    # Rotate x-axis labels
+    plt.xticks(rotation=45, ha='right')
+
+    # Use a locator for better spacing
+    ax3.xaxis.set_major_locator(MonthLocator())
+
+    plt.tight_layout()
 
     st.pyplot(fig3)
-
-
 
 def load_investor_details(investor):
     st.title(investor)
@@ -63,16 +73,20 @@ def load_investor_details(investor):
         big_series = df[df['investors'].str.contains(investor)].groupby('startup')['amount'].sum().sort_values(ascending=False).head()
         st.subheader('Biggest Investments')
         fig, ax = plt.subplots()
-        ax.bar(big_series.index,big_series.values)
+        ax.bar(big_series.index, big_series.values)
+        ax.set_xlabel('Startups')
+        ax.set_ylabel('Amount')
+        ax.set_title('Top Investments')
 
         st.pyplot(fig)
 
     with col2:
-        verical_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum()
+        vertical_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum()
 
         st.subheader('Sectors invested in')
         fig1, ax1 = plt.subplots()
-        ax1.pie(verical_series,labels=verical_series.index,autopct="%0.01f%%")
+        ax1.pie(vertical_series, labels=vertical_series.index, autopct="%0.01f%%")
+        ax1.set_title('Investments by Sector')
 
         st.pyplot(fig1)
 
@@ -83,23 +97,35 @@ def load_investor_details(investor):
 
     st.subheader('YoY Investment')
     fig2, ax2 = plt.subplots()
-    ax2.plot(year_series.index,year_series.values)
+    ax2.plot(year_series.index, year_series.values)
+    ax2.set_xlabel('Year')
+    ax2.set_ylabel('Total Amount Invested')
+    ax2.set_title('Year-wise Analysis')
+
+    # Rotate x-axis labels
+    plt.xticks(rotation=45, ha='right')
+
+    # Use a locator for better spacing
+    ax2.xaxis.set_major_locator(MonthLocator())
+
+    plt.tight_layout()
 
     st.pyplot(fig2)
 
 st.sidebar.title('Startup Funding Analysis')
 
-option = st.sidebar.selectbox('Select One',['Overall Analysis','StartUp','Investor'])
+option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'StartUp', 'Investor'])
 
 if option == 'Overall Analysis':
     load_overall_analysis()
 
 elif option == 'StartUp':
-    st.sidebar.selectbox('Select StartUp',sorted(df['startup'].unique().tolist()))
+    st.sidebar.selectbox('Select StartUp', sorted(df['startup'].unique().tolist()))
     btn1 = st.sidebar.button('Find StartUp Details')
     st.title('StartUp Analysis')
 else:
-    selected_investor = st.sidebar.selectbox('Select StartUp',sorted(set(df['investors'].str.split(',').sum())))
+    selected_investor = st.sidebar.selectbox('Select StartUp', sorted(set(df['investors'].str.split(',').sum())))
     btn2 = st.sidebar.button('Find Investor Details')
     if btn2:
         load_investor_details(selected_investor)
+
